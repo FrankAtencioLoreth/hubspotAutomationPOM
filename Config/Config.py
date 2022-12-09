a7df973d40714c0e9f56ce09988c4111e7595932
Frank Atencio <frank.atencio.loreth@gmail.com>
2022-12-09 11:47:53 -0500

    configuracion inicial

---
+++ Config/Config.py
@@ -0,0 +1,8 @@
+class TestData:
+
+    CHROME_EXECUTABLE_PATH = "Drivers/chromedriver"
+    FIREFOX_EXECUTABLE_PATH = "Drivers/geckodriver"
+    BASE_URL = "https://app.hubspot.com/login"
+
+    USER_NAME = "frank.atencio.loreth@gmail.com"
+    PASSWORD = "SeleniumAutomation@123"
